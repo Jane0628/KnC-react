d72f0659c4311b131168157b6071ee7315faa121
Jane <ribbon0628@naver.com>
2023-11-03 07:15:24 +0900

    23.11.03 | jh | 헤더 디자인 완성

--- src/page/Header.js
+++ src/page/Header.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import logoImg from "../pic/dark_logo.png"
+import PersonIcon from '@mui/icons-material/Person';
+
+// 디자인
+import design from "../design/Header.scss"
 
 const Header = () => {
-
   return (
     <header>
-      <div>
+      <div className='align'>
         <img src={logoImg} />
         <div className='right'>
-          <div>
+          <div className='users'>
+            <PersonIcon />
+            <a>My Page</a>
+            |
+            <a className='care'>CARE 회원이란?</a>
+          </div>
+          <div className='menus'>
+            <a>GUIDE</a>
+            <a>오디션</a>
+            <a>질문하기</a>
+            <a>CARE</a>
+            <a>파트너사</a>
           </div>
         </div>
       </div>
