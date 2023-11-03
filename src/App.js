4366776be25b26eb3e4993aaf2273b0355437fdb
Jane <ribbon0628@naver.com>
2023-11-03 22:24:05 +0900

    23.11.03 | jh | 호버 애니메이션 적용 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './page/Header';
+import Main from './page/Main';
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
--- src/page/Header.js
+++ src/page/Header.js
@@ -7,21 +7,23 @@ import design from "../design/Header.scss"
 
 const Header = () => {
   return (
-    <header>
+    <header className='suit'>
       <div className='align'>
         <img src={logoImg} />
         <div className='right'>
           <div className='users'>
-            <PersonIcon />
-            <a>My Page</a>
+            <div className='myPage'>
+              <PersonIcon />
+              <a>Login</a>
+            </div>
             |
             <a className='care'>CARE 회원이란?</a>
           </div>
           <div className='menus'>
-            <a>GUIDE</a>
+            <a className='english'>GUIDE</a>
             <a>오디션</a>
             <a>질문하기</a>
-            <a>CARE</a>
+            <a className='english'>CARE</a>
             <a>파트너사</a>
           </div>
         </div>
